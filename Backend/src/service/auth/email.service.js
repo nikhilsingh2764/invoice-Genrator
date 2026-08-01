332df74426b414a97d101c9647e096dc62fb6d1f
Nikhil Singh <nikhilsingh2764@gmail.com>
2026-08-01 11:40:44 +0000

    fix smtp configuration

--- Backend/src/service/auth/email.service.js
+++ Backend/src/service/auth/email.service.js
@@ -15,6 +15,9 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
+     tls: {
+        rejectUnauthorized: false
+    },
 
     connectionTimeout: 10000,
     greetingTimeout: 10000,
